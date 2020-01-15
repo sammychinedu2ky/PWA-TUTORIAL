7a4cc10e665c924ebd2468a5c4fd3ff73f880d7c
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-01-15 15:09:12 +0100

    setting up the service-worker.js file

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const cacheName = "pwatutorial-cache";
+const precache = ["/", "index.html", "style.css"];
+
+self.addEventListener("install", async event => {
+  console.log("Service worker install event!");
+  try {
+    let cache = await caches.open(cacheName);
+    cache.addAll(precache);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+self.addEventListener("activate", async event => {
+    const keys = await caches.keys();
+    keys.map(key =>key!=CACHE.name? caches.delete(key):'');
+  console.log("Service worker activate event!");
+});
+
+
+self.addEventListener("fetch", event => {
+  console.log("Fetch intercepted for:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
