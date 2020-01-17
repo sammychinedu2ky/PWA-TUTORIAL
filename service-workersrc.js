e3c3558ab7d11c860f5fdda28e99fbeef017fc2d
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-01-17 20:27:14 +0100

    created service-worker

---
+++ service-workersrc.js
@@ -0,0 +1,10 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Workbox is loaded`);
+
+  workbox.precaching.precacheAndRoute([]);
+
+} else {
+  console.log(`Error loading workbox`);
+}
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{png,html,json,js,css}"
+  ],
+  "swDest": "service-worker.js",
+  "swSrc": "service-workersrc.js"
+};
