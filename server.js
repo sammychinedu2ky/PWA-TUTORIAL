2b18d00f9146498b2a4c98a2b27af8306cabba1c
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-01-17 20:01:50 +0100

    process

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ let app = express()
 //this serves static files from the current directory
 app.use(express.static(__dirname));
 
-app.listen(3000,()=>{console.log('server running')})
+app.listen(process.env.PORT||3000,()=>{console.log('server running')})
